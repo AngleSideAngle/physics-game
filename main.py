70a51c2f67d77e571c9ce77ddb555985af8f552b
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-04-25 21:31:39 -0400

    fixed issues and added bad key inputs

--- main.py
+++ main.py
@@ -62,17 +62,20 @@ def main():
 
     while True:
         clock.tick(60)
+        user_input = [0,0]
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
-            if event.type == KEYDOWN and event.key == K_SPACE:
-                allsprites
+            if event.type == KEYDOWN and event.key == K_a:
+                user_input[0] -= 1
+            if event.type == KEYDOWN and event.key == K_d:
+                user_input[0] += 1
 
-        allsprites.update()
+        allsprites.update(user_input = user_input)
 
-        screen.blit(background, (0, 0))
+        screen.blit(background, (0,0))
         allsprites.draw(screen)
         pygame.display.flip()
         
